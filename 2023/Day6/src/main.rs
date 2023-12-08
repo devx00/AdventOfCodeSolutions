69764716948a33b58c73943b62af99fccad00de0
Zach Hanson <zachhanson94@gmail.com>
2023-12-07 20:43:38 -0800

    Day 6 Part 2 solved

--- 2023/Day6/src/main.rs
+++ 2023/Day6/src/main.rs
@@ -1,32 +1,29 @@
-fn num_ways_to_win(time: u32, record: u32) -> u32 {
-    (0..time).filter(|t| (time - t) * t > record).count() as u32
+fn num_ways_to_win(time: u64, record: u64) -> u64 {
+    (0..time).filter(|t| (time - t) * t > record).count() as u64
 }
 
 fn main() {
-    let input = include_str!("../inputs/part1.txt");
+    let input = include_str!("../inputs/part2.txt");
     let times_line = input.lines().nth(0).unwrap();
     let distance_line = input.lines().nth(1).unwrap();
-    let times: Vec<u32> = times_line
+    let time = times_line
         .strip_prefix("Time:")
         .expect("Times line not found.")
-        .split_whitespace()
-        .map(|x| x.parse().unwrap())
-        .collect();
-    let distances: Vec<u32> = distance_line
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
+
+    let distance = distance_line
         .strip_prefix("Distance:")
         .expect("Distances line not found.")
-        .split_whitespace()
-        .map(|x| x.parse().unwrap())
-        .collect();
+        .replace(" ", "")
+        .parse::<u64>()
+        .unwrap();
 
-    let answer = times
-        .iter()
-        .zip(distances.iter())
-        .map(|(t, d)| num_ways_to_win(*t, *d))
-        .fold(1, |acc, x| acc * x);
+    let answer = num_ways_to_win(time, distance);
 
-    println!("Times: {:?}", times);
-    println!("Distances: {:?}", distances);
+    println!("Times: {:?}", time);
+    println!("Distances: {:?}", distance);
     println!("Answer: {}", answer);
 
     ()
